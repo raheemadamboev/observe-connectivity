fae76925a651c14b0af7713fd1d75a997e2453c0
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-16 13:58:56 +0500

    add implementation for connectivity observer

---
+++ app/src/main/java/xyz/teamgravity/observeconnectivity/core/util/connectivity/ConnectivityObserverImp.kt
@@ -0,0 +1,44 @@
+package xyz.teamgravity.observeconnectivity.core.util.connectivity
+
+import android.net.ConnectivityManager
+import android.net.Network
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.launch
+
+class ConnectivityObserverImp(
+    private val manager: ConnectivityManager,
+) : ConnectivityObserver {
+
+    override fun observe(): Flow<ConnectivityObserver.Status> {
+        return callbackFlow {
+            val callback = object : ConnectivityManager.NetworkCallback() {
+                override fun onAvailable(network: Network) {
+                    super.onAvailable(network)
+                    launch { send(ConnectivityObserver.Status.Available) }
+                }
+
+                override fun onLosing(network: Network, maxMsToLive: Int) {
+                    super.onLosing(network, maxMsToLive)
+                    launch { send(ConnectivityObserver.Status.Losing) }
+                }
+
+                override fun onLost(network: Network) {
+                    super.onLost(network)
+                    launch { send(ConnectivityObserver.Status.Lost) }
+                }
+
+                override fun onUnavailable() {
+                    super.onUnavailable()
+                    launch { send(ConnectivityObserver.Status.Unavailable) }
+                }
+            }
+
+            manager.registerDefaultNetworkCallback(callback)
+
+            awaitClose { manager.unregisterNetworkCallback(callback) }
+        }.distinctUntilChanged()
+    }
+}
