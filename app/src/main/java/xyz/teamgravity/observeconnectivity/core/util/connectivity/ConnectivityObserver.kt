d63952af921d67d36f94d1c69bd6cc5ba68df96f
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-16 13:57:51 +0500

    add connectivity observer

---
+++ app/src/main/java/xyz/teamgravity/observeconnectivity/core/util/connectivity/ConnectivityObserver.kt
@@ -0,0 +1,11 @@
+package xyz.teamgravity.observeconnectivity.core.util.connectivity
+
+import kotlinx.coroutines.flow.Flow
+
+interface ConnectivityObserver {
+    fun observe(): Flow<Status>
+
+    enum class Status {
+        Available, Unavailable, Losing, Lost
+    }
+}
